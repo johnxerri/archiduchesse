bd3ec4ee6b7182ac3207bbe4b1049aba768e0494
Gary Chêne <gary.cne@gmail.com>
2016-07-01 11:10:28 +0200

    Modif routes

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,20 @@
 <?php
 	
 	$w_routes = array(
-		['GET', '/', 'Default#home', 'home'],
+
+		# Home¨Page et conditions de vente 
+		['GET', '/', 'Default#home', 'home'],	
+
+		# Page categories
+		['GET', '/categorie', 'Default#categorie', 'categorie'],
+
+		# Page detail 
+		['GET', '/detail', 'Default#detail', 'detail'],
+
+		# Page panier 
+		['GET', '/panier', 'Default#panier', 'panier'],
+
+		# Conditions de vente 
 		['GET', '/conditionsVente', 'Default#conditionsVente', 'conditionsVente'],
+		
 	);
